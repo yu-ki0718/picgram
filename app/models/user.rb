59b6c449654513f5bc9361e5166a64d5a50c6e42
岸波　雄樹 <you_work_24@icloud.com>
2019-01-27 16:39:10 +0900

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  validates :name,presence:true
-  validates :email,presence:true
+  validates :name,presence:true,length: { in: 1..15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,presence:true,format: VALID_EMAIL_REGEX
+  validates :password, format:/\A[[a-zA-Z0-9]]+\z/i,length: { in: 8..32 }
 
   has_secure_password
 end
+# 0-9a-zA-Z
+# ^[a-zA-Z0-9]+$
