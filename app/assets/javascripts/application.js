a4b4fcdcce3a279a11a2226903d99cc6ac13e291
岸波　雄樹 <you_work_24@icloud.com>
2019-03-09 17:53:50 +0900

    signup login add functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require bootstrap-sprockets
 //= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require activestorage
+//= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,20 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path,success: "登録が完了しました"
+      log_in @user
+      redirect_to topics_path,success: "登録が完了しました"
     else
       flash.now[:danger] = "登録に失敗しました"
       render :new
     end
-    redirect_to "/topics"
   end
 end
 
   private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
