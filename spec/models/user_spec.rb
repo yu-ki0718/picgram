a9f36cee5fa9eb7c0125b4a63c24aff4b69fc72d
岸波　雄樹 <you_work_24@icloud.com>
2019-03-26 15:55:46 +0900

    rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe User do
+    # 「メールの未設定時は無効となる」
+    it 'is invalid without a email' do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+    # 「アカウント名の未設定時は無効となる」
+    it 'is invalid without an account' do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
